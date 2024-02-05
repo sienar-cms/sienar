7c83b52dd560e99e5d7acb78b7255bcd8d588e81
Christian LeVesque <christian@levesque.dev>
2024-02-04 19:53:01 -0600

    added Prism component to wrap around PrismJS for code highlighting

---
+++ docs/Sienar.Docs.Plugin/Infrastructure/CodeType.cs
@@ -0,0 +1,9 @@
+﻿namespace Sienar.Infrastructure;
+
+public enum CodeType
+{
+	None,
+	Csharp,
+	Razor,
+	Css
+}
---
+++ docs/Sienar.Docs.Plugin/UI/Prism.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using Sienar.Infrastructure;
+
+namespace Sienar.UI;
+
+public class Prism : ComponentBase
+{
+	private ElementReference _codeBlock;
+	private string _codeClass = default!;
+
+	[Parameter]
+	public required RenderFragment ChildContent { get; set; }
+
+	[Parameter]
+	public CodeType Language { get; set; } = CodeType.None;
+
+	[Parameter]
+	public string Class { get; set; } = "my-8";
+
+	[Parameter]
+	public bool Block { get; set; }
+
+	[Inject]
+	private IJSRuntime Js { get; set; } = default!;
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		// <pre>
+		if (Block)
+		{
+			builder.OpenElement(0, "pre");
+			builder.AddAttribute(1, "class", Class);
+		}
+
+		// <code>
+		builder.OpenElement(2, "code");
+		builder.AddAttribute(3, "class", _codeClass);
+
+		// Set ref
+		builder.AddElementReferenceCapture(4, el => _codeBlock = el);
+
+		// Add child content
+		builder.AddContent(5, ChildContent);
+
+		// </code>
+		builder.CloseElement();
+
+		// </pre>
+		if (Block) builder.CloseElement();
+	}
+
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		_codeClass = CreateCssClasses();
+	}
+
+	/// <inheritdoc />
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		await Js.InvokeVoidAsync("Prism.highlightElement", _codeBlock);
+	}
+
+	private string CreateCssClasses()
+	{
+		return $"language-{Language.ToString().ToLowerInvariant()}";
+	}
+}
