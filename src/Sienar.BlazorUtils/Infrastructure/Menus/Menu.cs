82ee575536d1e42ad66ae0b13cfa67da17878950
Christian LeVesque <christian@levesque.dev>
2024-02-17 17:11:46 -0600

    deleted IMenu interface

--- src/Sienar.BlazorUtils/Infrastructure/Menus/Menu.cs
+++ src/Sienar.BlazorUtils/Infrastructure/Menus/Menu.cs
@@ -2,10 +2,14 @@
 
 namespace Sienar.Infrastructure.Menus;
 
-public class Menu : Dictionary<MenuPriority, List<MenuLink>>, IMenu
+public class Menu : Dictionary<MenuPriority, List<MenuLink>>
 {
-	/// <inheritdoc />
-	public IMenu AddMenuLink(
+	/// <summary>
+	/// Adds a navigation link to the active menu at the given priority
+	/// </summary>
+	/// <param name="menuLink">The nav link to add to the menu</param>
+	/// <param name="priority">The priority at which to add the nav link to the menu</param>
+	public Menu AddMenuLink(
 		MenuLink menuLink,
 		MenuPriority priority = MenuPriority.Mid)
 	{
