4c287e2efa7a1b9fd28741c43a49e6e8d1cd0bab
Christian LeVesque <christian@levesque.dev>
2024-03-19 15:19:17 -0500

    added checkbox component to Sienar.UI that sets the checked and unchecked colors to primary

---
+++ sienar/src/Sienar.UI/UI/Forms/Checkbox.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+// ReSharper disable once CheckNamespace
+namespace Sienar.UI;
+
+public class Checkbox<T> : MudCheckBox<T>
+{
+	public Checkbox()
+	{
+		Color = Color.Primary;
+		UnCheckedColor = Color.Primary;
+	}
+}
