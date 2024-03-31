c9b992c97fc3fe1ebab24ac32a907c97f6a697c1
Christian LeVesque <christian@levesque.dev>
2024-03-31 13:24:35 -0500

    moved SienarService into separate file from SienarStatusService

--- plugins/cms/src/Sienar.Plugin.Cms.Core/Infrastructure/Services/SienarService.cs
+++ plugins/cms/src/Sienar.Plugin.Cms.Core/Infrastructure/Services/SienarService.cs
@@ -7,37 +7,36 @@ using Sienar.Infrastructure.Processors;
 
 namespace Sienar.Infrastructure.Services;
 
-public class SienarStatusService<TRequest> : StatusService<TRequest>
+public class SienarService<TRequest, TResult> : Service<TRequest, TResult>
 {
 	private readonly IBotDetector _botDetector;
 
 	/// <inheritdoc />
-	public SienarStatusService(
-		ILogger<StatusService<TRequest>> logger,
+	public SienarService(
+		ILogger<Service<TRequest, TResult>> logger,
 		IEnumerable<IAccessValidator<TRequest>> accessValidators,
 		IEnumerable<IStateValidator<TRequest>> stateValidators,
 		IEnumerable<IBeforeProcess<TRequest>> beforeHooks,
 		IEnumerable<IAfterProcess<TRequest>> afterHooks,
-		IProcessor<TRequest, bool> processor,
+		IProcessor<TRequest, TResult> processor,
 		IBotDetector botDetector)
 		: base(
 			logger,
 			accessValidators,
 			stateValidators,
 			beforeHooks,
-			afterHooks, 
+			afterHooks,
 			processor)
 	{
 		_botDetector = botDetector;
 	}
 
 	/// <inheritdoc />
-	public override Task<bool> Execute(TRequest request)
+	public override Task<TResult?> Execute(TRequest request)
 	{
 		if (request is Honeypot honeypot && _botDetector.IsSpambot(honeypot))
 		{
-			// Silently short-circuit spambots
-			return Task.FromResult(true);
+			return Task.FromResult(default(TResult));
 		}
 
 		return base.Execute(request);
