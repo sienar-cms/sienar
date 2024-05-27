67dda6e11c0342af33ae99c6862beb3112ffdf86
Christian LeVesque <christian@levesque.dev>
2024-05-27 14:29:48 -0500

    added some missing xmldoc

--- plugins/cms/src/Sienar.Plugin.Cms.Blazor/Extensions/NavigationManagerExtensions.cs
+++ plugins/cms/src/Sienar.Plugin.Cms.Blazor/Extensions/NavigationManagerExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace Sienar.Extensions;
 
+/// <summary>
+/// Contains <see cref="NavigationManager"/> extension methods used by the <c>Sienar.Plugin.Cms.Blazor</c> assembly
+/// </summary>
 public static class NavigationManagerExtensions
 {
+	/// <summary>
+	/// Forces the page to reload pursuant to ASP.NET Core bug #51398
+	/// </summary>
+	/// <param name="self">the navigation manager</param>
+	/// <param name="destination">the URL that should </param>
 	public static void ForceReload(
 		this NavigationManager self,
 		string destination)
