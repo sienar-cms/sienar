a355cd788dc2ef634ede0b24e26fc9afb9400deb
Christian LeVesque <christian@levesque.dev>
2025-01-09 14:47:11 -0600

    added <SienarStartupActor> component that looks for IBeforeTask<SienarStartupActor> implementations and runs them at first render

---
+++ src/Sienar.Utils.Blazor/Components/SienarStartupActor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Sienar.Hooks;
+
+namespace Sienar.Components;
+
+/// <summary>
+/// A component that performs actions at application startup, after the Blazor UI has rendered
+/// </summary>
+public class SienarStartupActor : ComponentBase
+{
+	[Inject]
+	private IEnumerable<IBeforeTask<SienarStartupActor>> StartupActions { get; set; } = null!;
+
+	/// <inheritdoc />
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (!firstRender) return;
+
+		foreach (var action in StartupActions)
+		{
+			await action.Handle();
+		}
+	}
+}
