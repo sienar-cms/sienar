951a2b920486c4595682a7169ee3d9040901fb88
christianlevesque <christian@levesque.dev>
2025-01-19 17:39:26 -0600

    added users pages and processors

---
+++ src/Sienar.Plugin.Cms.Client/Identity/Processors/ClientLockUserAccountProcessor.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System.Threading.Tasks;
+using Sienar.Data;
+using Sienar.Identity.Requests;
+using Sienar.Infrastructure;
+using Sienar.Processors;
+
+namespace Sienar.Identity.Processors;
+
+/// <exclude />
+public class ClientLockUserAccountProcessor : IProcessor<LockUserAccountRequest, bool>
+{
+	private readonly IRestClient _client;
+
+	public ClientLockUserAccountProcessor(IRestClient client)
+	{
+		_client = client;
+	}
+
+	public Task<OperationResult<bool>> Process(LockUserAccountRequest request)
+		=> _client.Patch<bool>("users/lock", request);
+}
---
+++ src/Sienar.Plugin.Cms.Client/Identity/Processors/ClientRemoveUserFromRoleProcessor.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System.Threading.Tasks;
+using Sienar.Data;
+using Sienar.Identity.Requests;
+using Sienar.Infrastructure;
+using Sienar.Processors;
+
+namespace Sienar.Identity.Processors;
+
+/// <exclude />
+public class ClientRemoveUserFromRoleProcessor : IProcessor<RemoveUserFromRoleRequest, bool>
+{
+	private readonly IRestClient _client;
+
+	public ClientRemoveUserFromRoleProcessor(IRestClient client)
+	{
+		_client = client;
+	}
+
+	public Task<OperationResult<bool>> Process(RemoveUserFromRoleRequest request)
+		=> _client.Delete<bool>("users/roles", request);
+}
