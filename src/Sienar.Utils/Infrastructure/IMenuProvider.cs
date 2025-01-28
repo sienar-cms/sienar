810647dd1529dc99cd22680a498d5e3302689296
christianlevesque <christian@levesque.dev>
2025-01-28 17:07:10 -0600

    fixed XMLDoc comment

--- src/Sienar.Utils/Infrastructure/IMenuProvider.cs
+++ src/Sienar.Utils/Infrastructure/IMenuProvider.cs
@@ -3,6 +3,6 @@
 namespace Sienar.Infrastructure;
 
 /// <summary>
-/// The <see cref="IDictionaryProvider{T}"/> used to contain <see cref="MenuLink">menu links</see>
+/// The <see cref="IDictionaryProvider{TKey, TValue}"/> used to contain <see cref="MenuLink">menu links</see>
 /// </summary>
 public interface IMenuProvider : IDictionaryProvider<string, LinkDictionary<MenuLink>>;
