7d73c13e21542466fd4106169bcc8bb9611a50e4
Christian LeVesque <christian@levesque.dev>
2025-01-29 09:50:01 -0600

    updated menu provider to require Enum keys

--- src/Sienar.Utils/Infrastructure/IMenuProvider.cs
+++ src/Sienar.Utils/Infrastructure/IMenuProvider.cs
@@ -1,8 +1,8 @@
-﻿using Sienar.Infrastructure;
+﻿using System;
 
 namespace Sienar.Infrastructure;
 
 /// <summary>
 /// The <see cref="IDictionaryProvider{TKey, TValue}"/> used to contain <see cref="MenuLink">menu links</see>
 /// </summary>
-public interface IMenuProvider : IDictionaryProvider<string, LinkDictionary<MenuLink>>;
+public interface IMenuProvider : IDictionaryProvider<Enum, LinkDictionary<MenuLink>>;
