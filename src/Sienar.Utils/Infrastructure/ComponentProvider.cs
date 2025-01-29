9bd5edb08da02b154e334e80013a9ce1ac5e4d9d
Christian LeVesque <christian@levesque.dev>
2025-01-29 13:04:54 -0600

    added generic overload of IComponentProvider.Access that provides compile-time verification that the type provided inherits from LayoutComponentBase

--- src/Sienar.Utils/Infrastructure/ComponentProvider.cs
+++ src/Sienar.Utils/Infrastructure/ComponentProvider.cs
@@ -1,11 +1,16 @@
 ﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
 
 namespace Sienar.Infrastructure;
 
 /// <exclude />
 public class ComponentProvider
 	: DictionaryProvider<Type, ComponentDictionary>,
-		IComponentProvider;
+		IComponentProvider
+{
+	public ComponentDictionary Access<TLayout>()
+		where TLayout : LayoutComponentBase
+		=> Access(typeof(TLayout));
+}
