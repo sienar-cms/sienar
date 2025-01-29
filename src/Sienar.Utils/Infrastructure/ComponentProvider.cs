e3d99ce799d6d9142e42098bd0cf3f6872e617dc
Christian LeVesque <christian@levesque.dev>
2025-01-29 12:18:01 -0600

    added method to IComponentProvider to get a component by type and Enum ID, if it exists

--- src/Sienar.Utils/Infrastructure/ComponentProvider.cs
+++ src/Sienar.Utils/Infrastructure/ComponentProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System;
 using System.Collections.Generic;
 
 namespace Sienar.Infrastructure;
@@ -6,4 +8,12 @@ namespace Sienar.Infrastructure;
 /// <exclude />
 public class ComponentProvider
 	: DictionaryProvider<Type, Dictionary<Enum, Type>>,
-		IComponentProvider;
+		IComponentProvider
+{
+	public Type? GetComponent(Type type, Enum id)
+	{
+		if (!TryGetValue(type, out var components)) return null;
+		components.TryGetValue(id, out var component);
+		return component;
+	}
+}
--- src/Sienar.Utils/Infrastructure/IComponentProvider.cs
+++ src/Sienar.Utils/Infrastructure/IComponentProvider.cs
@@ -6,4 +6,13 @@ namespace Sienar.Infrastructure;
 /// <summary>
 /// A provider to contain references to various components to render in the Sienar UI
 /// </summary>
-public interface IComponentProvider : IDictionaryProvider<Type, Dictionary<Enum, Type>>;
+public interface IComponentProvider : IDictionaryProvider<Type, Dictionary<Enum, Type>>
+{
+	/// <summary>
+	/// Gets a component from the component provider using the provided layout type and component ID
+	/// </summary>
+	/// <param name="type">The type of the layout that renders the component</param>
+	/// <param name="id">The ID of the layout section defined by the component</param>
+	/// <returns>The component type, if one is found</returns>
+	Type? GetComponent(Type type, Enum id);
+}
