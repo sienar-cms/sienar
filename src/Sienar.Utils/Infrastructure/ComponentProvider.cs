f476bd5a99d60de3343e171beeec456810d174dd
Christian LeVesque <christian@levesque.dev>
2025-01-29 12:54:51 -0600

    changed component provider to use new ComponentDictionary, which has some fluent methods to make registering components more succinct

--- src/Sienar.Utils/Infrastructure/ComponentProvider.cs
+++ src/Sienar.Utils/Infrastructure/ComponentProvider.cs
@@ -7,13 +7,5 @@ namespace Sienar.Infrastructure;
 
 /// <exclude />
 public class ComponentProvider
-	: DictionaryProvider<Type, Dictionary<Enum, Type>>,
-		IComponentProvider
-{
-	public Type? GetComponent(Type type, Enum id)
-	{
-		if (!TryGetValue(type, out var components)) return null;
-		components.TryGetValue(id, out var component);
-		return component;
-	}
-}
+	: DictionaryProvider<Type, ComponentDictionary>,
+		IComponentProvider;
