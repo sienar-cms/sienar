dc49deb677f3e491e5bf336f1cf37060439c66fa
christianlevesque <christian@levesque.dev>
2025-02-02 12:57:01 -0600

    added basic base component for all Sienar UI components

---
+++ src/Sienar.Utils/Ui/SienarComponentBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A base component with functionality common across most Sienar components
+/// </summary>
+public class SienarComponentBase : ComponentBase
+{
+	/// <summary>
+	/// Captures all attributes passed to a component and forwards them to the component
+	/// </summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public Dictionary<string, object>? Attributes { get; set; }
+
+	/// <summary>
+	/// Adds a CSS class to the <see cref="Attributes"/> dictionary while preserving any existing values
+	/// </summary>
+	/// <param name="className">The CSS class name to add</param>
+	protected void AddCssClass(string className)
+	{
+		if (Attributes is null) return;
+
+		if (Attributes.TryGetValue("class", out var classes))
+		{
+			Attributes["class"] = $"{classes} {className}";
+		}
+		else
+		{
+			Attributes["class"] = className;
+		}
+	}
+}
