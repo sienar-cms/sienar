60b850dba065fce9a51d2ff98d265b734e403298
christianlevesque <christian@levesque.dev>
2025-02-03 15:21:57 -0600

    added <HtmlElement> component for rendering dynamic HTML tags

---
+++ src/Sienar.Utils/Ui/HtmlElement.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A component used to render an HTML element dynamically
+/// </summary>
+public class HtmlElement : SienarComponentBase
+{
+	/// <summary>
+	/// The HTML tag to render
+	/// </summary>
+	[Parameter]
+	public required string Tag { get; set; }
+
+	/// <summary>
+	/// The child content to render, if any
+	/// </summary>
+	[Parameter]
+	public RenderFragment? ChildContent { get; set; }
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		builder.OpenElement(0, Tag);
+		builder.AddMultipleAttributes(1, Attributes);
+		builder.AddContent(2, ChildContent);
+		builder.CloseElement();
+	}
+}
