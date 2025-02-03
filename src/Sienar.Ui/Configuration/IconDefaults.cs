f4b56d01d0c6927e6287f4a520eba287a3861a8e
christianlevesque <christian@levesque.dev>
2025-02-02 18:52:51 -0600

    added <Icon> component with defaults

---
+++ src/Sienar.Ui/Configuration/IconDefaults.cs
@@ -0,0 +1,24 @@
+﻿using Sienar.Ui;
+
+namespace Sienar.Configuration;
+
+/// <summary>
+/// Contains configurable default parameter values for icons
+/// </summary>
+public static class IconDefaults
+{
+	/// <summary>
+	/// The default icon style
+	/// </summary>
+	public static IconStyle IconStyle { get; set; } = IconStyle.Solid;
+
+	/// <summary>
+	/// The default icon size
+	/// </summary>
+	public static Size Size { get; set; } = Size.Normal;
+
+	/// <summary>
+	/// The default icon color
+	/// </summary>
+	public static Color Color { get; set; } = Color.Inherit;
+}
---
+++ src/Sienar.Ui/Ui/Icon.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Sienar.Configuration;
+using Sienar.Extensions;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A Bulma-styled HTML icon
+/// </summary>
+/// <remarks>
+/// <para>
+/// This component represents a Bulma-styled HTML icon using FontAwesome icons. It accepts parameters for the FontAwesome <see cref="IconStyle"/>, the Bulma <see cref="Size"/>, and the Bulma <see cref="Color"/>, as well as a <c>string</c> argument representing the name of the FontAwesome icon to render (without the <c>"fa-"</c> prefix).
+/// </para>
+///
+/// <para>
+/// However, it doesn't represent all possible use cases for icons. For example, the Bulma docs describe <see href="https://bulma.io/documentation/elements/icon/#icon-text">using icons with text</see>. This is not supported directly by this component because this component is only an implementation of the <c>&lt;span class="icon"&gt;</c> portion. Developers are still free to use the icon text technique in Bulma by using this component to mark up the icon itself, then manually creating the rest of the markup.
+/// </para>
+/// </remarks>
+/// <example>Creating a standalone icon:
+/// <code>
+/// &lt;Icon
+///     Name="home"
+///     IconStyle="IconStyle.Thin"
+///     Size="Size.Large"/&gt;
+/// </code>
+/// </example>
+/// <example>Creating an icon with accompanying text:
+/// <code>
+/// // YourComponent.razor
+/// &lt;span class="icon-text"&gt;
+///   &lt;Icon Name="home"/&gt;
+///   &lt;span&gt;Home&lt;/span&gt;
+/// &lt;/span&gt;
+/// </code>
+/// </example>
+public class Icon : SienarComponentBase
+{
+	/// <summary>
+	/// The icon style
+	/// </summary>
+	[Parameter]
+	public IconStyle IconStyle { get; set; } = IconDefaults.IconStyle;
+
+	/// <summary>
+	/// The icon size
+	/// </summary>
+	[Parameter]
+	public Size Size { get; set; } = IconDefaults.Size;
+
+	/// <summary>
+	/// The icon color
+	/// </summary>
+	[Parameter]
+	public Color Color { get; set; } = IconDefaults.Color;
+
+	/// <summary>
+	/// The name of the icon to use, <b>without</b> the "fa-" prefix
+	/// </summary>
+	[Parameter]
+	public required string Name { get; set; }
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		MapClasses();
+
+		builder.OpenElement(0, "span");
+		builder.AddMultipleAttributes(1, Attributes);
+		builder.AddContent(2, BuildIcon);
+		builder.CloseElement();
+	}
+
+	private void BuildIcon(RenderTreeBuilder builder)
+	{
+		builder.OpenElement(0, "i");
+		builder.AddAttribute(
+			1,
+			"class",
+			$"fa-{IconStyle.GetHtmlValue()} fa-{Name}");
+		builder.CloseElement();
+	}
+
+	private void MapClasses()
+	{
+		var classes = $"icon {Size.GetHtmlValue()} has-text-{Color.GetHtmlValue()}";
+		AddCssClass(classes);
+	}
+}
