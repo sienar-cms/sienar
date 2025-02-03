0dc88c27cdda7c82b6207f121ec2d99ef5550759
christianlevesque <christian@levesque.dev>
2025-02-03 10:36:41 -0600

    added <ProgressBar> component with defaults

---
+++ src/Sienar.Ui/Configuration/ProgressBarDefaults.cs
@@ -0,0 +1,24 @@
+﻿using Sienar.Ui;
+
+namespace Sienar.Configuration;
+
+/// <summary>
+/// Contains configurable default parameter values for progress bars
+/// </summary>
+public static class ProgressBarDefaults
+{
+	/// <summary>
+	/// The default progress bar color
+	/// </summary>
+	public static Color Color { get; set; } = Color.Primary;
+
+	/// <summary>
+	/// the default progress bar size
+	/// </summary>
+	public static Size Size { get; set; } = Size.Normal;
+
+	/// <summary>
+	/// The default max value
+	/// </summary>
+	public static float Max { get; set; } = 100;
+}
---
+++ src/Sienar.Ui/Ui/ProgressBar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Sienar.Configuration;
+using Sienar.Extensions;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A Bulma-styled HTML &lt;progress&gt; element
+/// </summary>
+public class ProgressBar : SienarComponentBase
+{
+	/// <summary>
+	/// The color of the progess bar
+	/// </summary>
+	[Parameter]
+	public Color Color { get; set; } = ProgressBarDefaults.Color;
+
+	/// <summary>
+	/// The size of the progress bar
+	/// </summary>
+	[Parameter]
+	public Size Size { get; set; } = ProgressBarDefaults.Size;
+
+	/// <summary>
+	/// The value of the HTML <c>value</c> attribute
+	/// </summary>
+	[Parameter]
+	public float? Value { get; set; }
+
+	/// <summary>
+	/// The value of the HTML <c>max</c> attribute
+	/// </summary>
+	[Parameter]
+	public float Max { get; set; } = ProgressBarDefaults.Max;
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		Attributes ??= new Dictionary<string, object>();
+		var progress = 0;
+
+		if (Value.HasValue)
+		{
+			Attributes["value"] = Value.Value;
+			Attributes["max"] = Max;
+			progress = (int)Math.Floor(Value.Value / Max * 100);
+		}
+
+		MapClasses();
+
+		builder.OpenElement(0, "progress");
+		builder.AddMultipleAttributes(1, Attributes);
+		builder.AddContent(2, $"{progress}%");
+		builder.CloseElement();
+	}
+
+	private void MapClasses()
+	{
+		var classes = $"progress is-{Color.GetHtmlValue()} {Size.GetHtmlValue()}";
+		AddCssClass(classes);
+	}
+}
