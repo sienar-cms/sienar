3cc4f4f9ba50237ad64bba25a3c3d5a97a898fbd
christianlevesque <christian@levesque.dev>
2025-02-02 18:34:51 -0600

    added FontAwesome icon style enum

---
+++ src/Sienar.Ui/Ui/IconStyle.cs
@@ -0,0 +1,39 @@
+﻿using Sienar.Infrastructure;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// Represents FontAwesome icon styles
+/// </summary>
+public enum IconStyle
+{
+	/// <summary>
+	/// A solid icon style (i.e., "fa-solid")
+	/// </summary>
+	[HtmlValue("solid")]
+	Solid,
+
+	/// <summary>
+	/// A regular icon style (i.e., "fa-regular")
+	/// </summary>
+	[HtmlValue("regular")]
+	Regular,
+
+	/// <summary>
+	/// A light icon style (i.e., "fa-light")
+	/// </summary>
+	[HtmlValue("light")]
+	Light,
+
+	/// <summary>
+	/// A thin icon style (i.e., "fa-thin")
+	/// </summary>
+	[HtmlValue("thin")]
+	Thin,
+
+	/// <summary>
+	/// A brand icon style (i.e., "fa-brands")
+	/// </summary>
+	[HtmlValue("brands")]
+	Brands
+}
