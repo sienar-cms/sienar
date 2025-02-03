f69a4c948a18142dd344ae833332c8fc3442675c
christianlevesque <christian@levesque.dev>
2025-02-02 18:58:56 -0600

    added <Button> component with defaults

---
+++ src/Sienar.Ui/Configuration/ButtonDefaults.cs
@@ -0,0 +1,44 @@
+﻿using Sienar.Ui;
+
+namespace Sienar.Configuration;
+
+/// <summary>
+/// Contains configurable default parameter values for buttons
+/// </summary>
+public static class ButtonDefaults
+{
+	/// <summary>
+	/// The default button color
+	/// </summary>
+	public static Color Color { get; set; } = Color.Primary;
+
+	/// <summary>
+	/// The default button size
+	/// </summary>
+	public static Size Size { get; set; } = Size.Normal;
+
+	/// <summary>
+	/// Whether buttons should be responsive by default
+	/// </summary>
+	public static bool Responsive { get; set; } = false;
+
+	/// <summary>
+	/// Whether buttons should be full-width by default
+	/// </summary>
+	public static bool FullWidth { get; set; } = false;
+
+	/// <summary>
+	/// Whether buttons should be light by default
+	/// </summary>
+	public static bool Light { get; set; } = false;
+
+	/// <summary>
+	/// Whether buttons should be dark by default
+	/// </summary>
+	public static bool Dark { get; set; } = false;
+
+	/// <summary>
+	/// Whether buttons should be outlined by default
+	/// </summary>
+	public static bool Outlined { get; set; } = false;
+}
---
+++ src/Sienar.Ui/Ui/Button.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Sienar.Configuration;
+using Sienar.Extensions;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A Bulma-styled HTML <c>&lt;button&gt;</c> element
+/// </summary>
+public class Button : SienarComponentBase
+{
+	private string _tag = "button";
+
+	/// <summary>
+	/// The theme color of the button
+	/// </summary>
+	[Parameter]
+	public Color Color { get; set; } = ButtonDefaults.Color;
+
+	/// <summary>
+	/// The size of the button
+	/// </summary>
+	[Parameter]
+	public Size Size { get; set; } = ButtonDefaults.Size;
+
+	/// <summary>
+	/// Whether the button should be responsive
+	/// </summary>
+	[Parameter]
+	public bool Responsive { get; set; } = ButtonDefaults.Responsive;
+
+	/// <summary>
+	/// Whether the button should be full-width
+	/// </summary>
+	[Parameter]
+	public bool FullWidth { get; set; } = ButtonDefaults.FullWidth;
+
+	/// <summary>
+	/// Whether the button should be light
+	/// </summary>
+	[Parameter]
+	public bool Light { get; set; } = ButtonDefaults.Light;
+
+	/// <summary>
+	/// Whether the button should be dark
+	/// </summary>
+	[Parameter]
+	public bool Dark { get; set; } = ButtonDefaults.Dark;
+
+	/// <summary>
+	/// Whether the button should be outlined
+	/// </summary>
+	[Parameter]
+	public bool Outlined { get; set; } = ButtonDefaults.Outlined;
+
+	/// <summary>
+	/// Whether the button is in a loading state
+	/// </summary>
+	[Parameter]
+	public bool Loading { get; set; }
+
+	/// <summary>
+	/// The child content
+	/// </summary>
+	[Parameter]
+	public required RenderFragment? ChildContent { get; set; }
+
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		if (Attributes is null) return;
+
+		if (Attributes.ContainsKey("href"))
+		{
+			_tag = "a";
+		} else if (Attributes.ContainsKey("type"))
+		{
+			_tag = "input";
+		}
+	}
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		MapClasses();
+
+		builder.OpenElement(0, _tag);
+		builder.AddMultipleAttributes(1, Attributes);
+		builder.AddContent(2, ChildContent);
+		builder.CloseElement();
+	}
+
+	private void MapClasses()
+	{
+		var classes = $"button is-{Color.GetHtmlValue()} {Size.GetHtmlValue()}";
+
+		if (Light) classes += " is-light";
+		else if (Dark) classes += " is-dark";
+
+		if (Responsive) classes += " is-responsive";
+		if (FullWidth) classes += " is-fullwidth";
+		if (Outlined) classes += " is-outlined";
+		if (Loading) classes += " is-loading";
+
+		AddCssClass(classes);
+	}
+}
