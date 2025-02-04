149fe76a3f64cc96593500aa1fac143be435d9d8
christianlevesque <christian@levesque.dev>
2025-02-04 11:07:39 -0600

    added <ButtonGroup> component with defaults

---
+++ src/Sienar.Ui/Configuration/ButtonGroupDefaults.cs
@@ -0,0 +1,29 @@
+﻿using Sienar.Ui;
+
+namespace Sienar.Configuration;
+
+/// <summary>
+/// Contains configurable default parameter values for button containers
+/// </summary>
+public static class ButtonGroupDefaults
+{
+	/// <summary>
+	/// The default button container HTML tag
+	/// </summary>
+	public static string Tag { get; set; } = "div";
+
+	/// <summary>
+	/// The default button size of contained buttons
+	/// </summary>
+	public static Size? Size { get; set; } = null; 
+
+	/// <summary>
+	/// The default button alignment
+	/// </summary>
+	public static Alignment Alignment { get; set; } = Alignment.Left;
+
+	/// <summary>
+	/// Whether button containers should have addons by default
+	/// </summary>
+	public static bool HasAddons { get; set; }
+}
---
+++ src/Sienar.Ui/Ui/ButtonGroup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Sienar.Configuration;
+using Sienar.Extensions;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// A Bulma-styled HTML button wrapper
+/// </summary>
+public class ButtonGroup : SienarComponentBase
+{
+	/// <summary>
+	/// The HTML tag with which to render the button container
+	/// </summary>
+	[Parameter]
+	public string Tag { get; set; } = ButtonGroupDefaults.Tag;
+
+	/// <summary>
+	/// The default size of the contained buttons
+	/// </summary>
+	/// <remarks>
+	/// This value can still be overridden on a specific button by providing a <c>Size</c> parameter to that button.
+	/// </remarks>
+	[Parameter]
+	public Size? Size { get; set; } = ButtonGroupDefaults.Size;
+
+	/// <summary>
+	/// The alignment of the contained buttons
+	/// </summary>
+	[Parameter]
+	public Alignment Alignment { get; set; } = ButtonGroupDefaults.Alignment;
+
+	/// <summary>
+	/// Whether the button container should render buttons as addons
+	/// </summary>
+	[Parameter]
+	public bool HasAddons { get; set; } = ButtonGroupDefaults.HasAddons;
+
+	/// <summary>
+	/// The child content to render
+	/// </summary>
+	[Parameter]
+	public required RenderFragment ChildContent { get; set; }
+
+	/// <inheritdoc />
+	protected override void BuildRenderTree(RenderTreeBuilder builder)
+	{
+		builder.OpenElement(0, Tag);
+		builder.AddMultipleAttributes(1, Attributes);
+		builder.AddAttribute(2, "class", CreateCssClasses());
+		builder.AddContent(3, ChildContent);
+		builder.CloseElement();
+	}
+
+	private string CreateCssClasses()
+	{
+		var classes = "buttons";
+
+		if (Size.HasValue) classes += $" are-{Size.GetHtmlValue()}";
+		if (Alignment > Alignment.Left) classes += $" is-{Alignment.GetHtmlValue()}";
+		if (HasAddons) classes += " has-addons";
+
+		return MergeCssClasses(classes);
+	}
+}
