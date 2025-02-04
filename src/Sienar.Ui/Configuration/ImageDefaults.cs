bd702371449ae6717192aa1d35ed81668ca110d5
christianlevesque <christian@levesque.dev>
2025-02-04 14:09:18 -0600

    added <Image> component with defaults

---
+++ src/Sienar.Ui/Configuration/ImageDefaults.cs
@@ -0,0 +1,19 @@
+﻿using Sienar.Ui;
+
+namespace Sienar.Configuration;
+
+/// <summary>
+/// Contains configurable default parameter values for images
+/// </summary>
+public static class ImageDefaults
+{
+	/// <summary>
+	/// The default image size
+	/// </summary>
+	public static ImageSize? ImageSize { get; set; } = null;
+
+	/// <summary>
+	/// Whether images should be rounded by default
+	/// </summary>
+	public static bool Rounded { get; set; }
+}
