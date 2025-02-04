464adfd157d9c54476a8d53fb5d692b2a237b080
christianlevesque <christian@levesque.dev>
2025-02-04 11:07:26 -0600

    added Alignment enum

---
+++ src/Sienar.Ui/Ui/Alignment.cs
@@ -0,0 +1,27 @@
+﻿using Sienar.Infrastructure;
+
+namespace Sienar.Ui;
+
+/// <summary>
+/// Represents horizontal alignment values
+/// </summary>
+public enum Alignment
+{
+	/// <summary>
+	/// Horizontally left-aligned. Since this is the default alignment in Bulma, it generally does not apply a CSS class except in specific circumstances
+	/// </summary>
+	[HtmlValue("")]
+	Left,
+
+	/// <summary>
+	/// Horizontally centered alignment
+	/// </summary>
+	[HtmlValue("centered")]
+	Centered,
+
+	/// <summary>
+	/// Horizontally right-aligned
+	/// </summary>
+	[HtmlValue("right")]
+	Right
+}
