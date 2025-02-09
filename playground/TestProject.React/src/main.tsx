6a6fbcf5e31ff245e1eae561fdd4e8c30710f155
Christian LeVesque <christian@levesque.dev>
2025-02-09 16:28:29 -0600

    made overrides file a .tsx file so it can override views as well

--- playground/TestProject.React/src/main.tsx
+++ playground/TestProject.React/src/main.tsx
@@ -1,6 +1,6 @@
 ﻿import { createApp, DOCUMENT_TITLE_SUFFIX, provide, setup as reactUtilsSetup } from '@sienar/react-utils';
 import { setup as reactUiSetup } from '@sienar/react-ui-mui';
-import './overrides.ts';
+import './overrides.tsx';
 import { setup } from '@sienar/react-client-mui';
 
 provide(DOCUMENT_TITLE_SUFFIX, '| Sienar Development App');
