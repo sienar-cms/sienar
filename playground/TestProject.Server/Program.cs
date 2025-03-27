14029439ba93b65d6d0c44ed7f2664683978b2ad
christianlevesque <christian@levesque.dev>
2025-03-26 19:55:18 -0500

    deleted Blazor code

--- playground/TestProject.Server/Program.cs
+++ playground/TestProject.Server/Program.cs
@@ -2,12 +2,10 @@ using Microsoft.AspNetCore.Builder;
 using Sienar.Extensions;
 using Sienar.Infrastructure;
 using TestProject;
-using TestProject.Client;
 
 await SienarAppBuilder
 	.Create(args)
 	.AddWebAdapter()
 	.AddPlugin<TestProjectServerPlugin>()
-	.AddPlugin<TestProjectClientPlugin>()
 	.Build<WebApplication>()
 	.RunAsync();
