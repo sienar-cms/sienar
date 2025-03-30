dd6c50a4fbe366eaa27d83a59733a946dec0038a
christianlevesque <christian@levesque.dev>
2025-03-30 03:58:14 -0500

    removed SienarDbContext, moved entity configurations to Sienar.Plugin.Cms, and added an extension method to apply entity configurations against a ModelBuilder

---
+++ src/Sienar.Plugin.Cms.Server/Extensions/SienarCmsModelBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sienar.Identity.Data;
+using Sienar.Media.Data;
+
+namespace Sienar.Extensions;
+
+/// <summary>
+/// Contains <see cref="ModelBuilder"/> extension methods for the <c>Sienar.Plugin.Cms</c> assembly
+/// </summary>
+public static class SienarCmsModelBuilderExtensions
+{
+	/// <summary>
+	/// Adds Sienar's entity configurations to the model builder
+	/// </summary>
+	/// <param name="b">The model builder</param>
+	/// <returns>The model builder</returns>
+	public static ModelBuilder AddSienarCmsModels(
+		this ModelBuilder b)
+	{
+		return b
+			.ApplyConfiguration(
+				new SienarUserEntityConfigurer())
+			.ApplyConfiguration(
+				new LockoutReasonEntityConfigurer())
+			.ApplyConfiguration(
+				new UploadEntityConfigurer());
+	}
+}
