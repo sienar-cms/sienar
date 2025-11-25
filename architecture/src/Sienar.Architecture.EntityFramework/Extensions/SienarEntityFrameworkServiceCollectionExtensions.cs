8e56864a1885dd98e1030e972b881d255b213a3d
christianlevesque <christian@levesque.dev>
2025-11-25 09:45:04 -0600

    removed extraneous extension methods

--- architecture/src/Sienar.Architecture.EntityFramework/Extensions/SienarEntityFrameworkServiceCollectionExtensions.cs
+++ architecture/src/Sienar.Architecture.EntityFramework/Extensions/SienarEntityFrameworkServiceCollectionExtensions.cs
@@ -12,36 +12,6 @@ public static class SienarEntityFrameworkServiceCollectionExtensions
 	/// <summary>
 	/// Adds the necessary services to use an entity via an EF repository
 	/// </summary>
-	/// <param name="self">the service collection</param>
-	/// <typeparam name="TEntity">the type of the entity</typeparam>
-	/// <typeparam name="TFilterProcessor">the type of the filter processor</typeparam>
-	/// <typeparam name="TContext">the type of the database context</typeparam>
-	/// <returns>the service collection</returns>
-	public static IServiceCollection AddEntityFrameworkEntityWithDefaultRepository<TEntity, TFilterProcessor, TContext>(
-		this IServiceCollection self)
-		where TEntity : EntityBase
-		where TFilterProcessor : class, IEntityFrameworkFilterProcessor<TEntity>
-		where TContext : DbContext
-		=> AddEntityFrameworkEntity<TEntity, TFilterProcessor, EntityFrameworkRepository<TEntity, TContext>>(self);
-
-	/// <summary>
-	/// Adds the necessary services to use an entity via an EF repository
-	/// </summary>
-	/// <param name="self">the service collection</param>
-	/// <typeparam name="TEntity">the type of the entity</typeparam>
-	/// <typeparam name="TFilterProcessor">the type of the filter processor</typeparam>
-	/// <typeparam name="TRepository">the implementation type of the repository</typeparam>
-	/// <returns>the service collection</returns>
-	public static IServiceCollection AddEntityFrameworkEntity<TEntity, TFilterProcessor, TRepository>(
-		this IServiceCollection self)
-		where TEntity : EntityBase
-		where TFilterProcessor : class, IEntityFrameworkFilterProcessor<TEntity>
-		where TRepository : class, IRepository<TEntity>
-		=> AddEntityFrameworkEntity<TEntity, TFilterProcessor, IRepository<TEntity>, TRepository>(self);
-
-	/// <summary>
-	/// Adds the necessary services to use an entity via an EF repository
-	/// </summary>
 	/// <param name="self">The service collection</param>
 	/// <typeparam name="TEntity">The type of the entity</typeparam>
 	/// <typeparam name="TFilterProcessor">The type of the filter processor</typeparam>
