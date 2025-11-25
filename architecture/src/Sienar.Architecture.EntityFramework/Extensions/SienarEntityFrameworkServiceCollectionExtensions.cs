8c4ee02df1043e8c91a6c5037bbe2c5d078d43e5
christianlevesque <christian@levesque.dev>
2025-11-25 09:44:49 -0600

    refactored AddEntityFrameworkEntity to add EF implementation of IEntityReader

--- architecture/src/Sienar.Architecture.EntityFramework/Extensions/SienarEntityFrameworkServiceCollectionExtensions.cs
+++ architecture/src/Sienar.Architecture.EntityFramework/Extensions/SienarEntityFrameworkServiceCollectionExtensions.cs
@@ -42,29 +42,21 @@ public static class SienarEntityFrameworkServiceCollectionExtensions
 	/// <summary>
 	/// Adds the necessary services to use an entity via an EF repository
 	/// </summary>
-	/// <param name="self">the service collection</param>
-	/// <typeparam name="TEntity">the type of the entity</typeparam>
-	/// <typeparam name="TFilterProcessor">the type of the filter processor</typeparam>
-	/// <typeparam name="TRepository">the service type of the repository</typeparam>
-	/// <typeparam name="TRepositoryImplementation">the implementation type of the repository</typeparam>
-	/// <returns>the service collection</returns>
-	public static IServiceCollection AddEntityFrameworkEntity<TEntity, TFilterProcessor, TRepository, TRepositoryImplementation>(
+	/// <param name="self">The service collection</param>
+	/// <typeparam name="TEntity">The type of the entity</typeparam>
+	/// <typeparam name="TFilterProcessor">The type of the filter processor</typeparam>
+	/// <typeparam name="TContext">The type of the filter processor</typeparam>
+	/// <returns>The service collection</returns>
+	public static IServiceCollection AddEntityFrameworkEntity<TEntity, TFilterProcessor, TContext>(
 		this IServiceCollection self)
 		where TEntity : EntityBase
 		where TFilterProcessor : class, IEntityFrameworkFilterProcessor<TEntity>
-		where TRepository : class, IRepository<TEntity>
-		where TRepositoryImplementation : class, TRepository
+		where TContext : DbContext
 	{
 		self.TryAddScoped<IBeforeAction<TEntity>, ConcurrencyStampUpdater<TEntity>>();
-		self.TryAddScoped<IStateValidator<TEntity>, ConcurrencyStampValidator<TEntity>>();
+		self.TryAddScoped<IStateValidator<TEntity>, ConcurrencyStampValidator<TEntity, TContext>>();
 		self.TryAddScoped<IEntityFrameworkFilterProcessor<TEntity>, TFilterProcessor>();
-		self.TryAddScoped<TRepository, TRepositoryImplementation>();
-
-		if (typeof(TRepository) != typeof(IRepository<TEntity>))
-		{
-			self.AddScoped<IRepository<TEntity>>(
-				sp => sp.GetRequiredService<TRepository>());
-		}
+		self.TryAddScoped<IEntityReader<TEntity>, EfEntityReader<TEntity, TContext>>();
 
 		return self;
 	}
