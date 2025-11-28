45d177f6beb0bf3b6663608e634d4d5407b2ccf6
christianlevesque <christian@levesque.dev>
2025-11-28 15:05:44 -0600

    added IMapper for mapping between DTOs and entities

---
+++ shared/src/Sienar.Utils/Data/IMapper.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable TypeParameterCanBeVariant
+namespace Sienar.Data;
+
+/// <summary>
+/// Maps between entities and their associated DTOs
+/// </summary>
+/// <typeparam name="TDto">The type of the DTO</typeparam>
+/// <typeparam name="TEntity">The type of the entity</typeparam>
+public interface IMapper<TDto, TEntity>
+	where TDto : class
+	where TEntity : EntityBase
+{
+	/// <summary>
+	/// Maps a DTO to its associated entity
+	/// </summary>
+	/// <param name="dto">The DTO to map</param>
+	/// <param name="entity">The entity to which to map</param>
+	void MapToEntity(TDto dto, TEntity entity);
+
+	/// <summary>
+	/// Maps an entity to its associated DTO
+	/// </summary>
+	/// <param name="dto">The DTO to which to map</param>
+	/// <param name="entity">The entity to map</param>
+	void MapToDto(TDto dto, TEntity entity);
+}
