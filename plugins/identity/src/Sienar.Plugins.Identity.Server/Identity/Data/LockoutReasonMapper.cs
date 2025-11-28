f3e48afe62a6e9a9cb6c4fd82b044b71943642ac
christianlevesque <christian@levesque.dev>
2025-11-28 15:43:08 -0600

    added LockoutReasonDto and mapper

---
+++ plugins/identity/src/Sienar.Plugins.Identity.Server/Identity/Data/LockoutReasonMapper.cs
@@ -0,0 +1,23 @@
+﻿using Sienar.Data;
+using Sienar.Extensions;
+
+namespace Sienar.Identity.Data;
+
+/// <summary>
+/// Maps between <see cref="LockoutReasonDto"/> and <see cref="LockoutReason"/>
+/// </summary>
+public class LockoutReasonMapper : IMapper<LockoutReasonDto, LockoutReason>
+{
+	/// <inheritdoc />
+	public void MapToEntity(LockoutReasonDto dto, LockoutReason entity)
+	{
+		entity.Reason = dto.Reason;
+		entity.NormalizedReason = dto.Reason.ToNormalized();
+	}
+
+	/// <inheritdoc />
+	public void MapToDto(LockoutReasonDto dto, LockoutReason entity)
+	{
+		dto.Reason = entity.Reason;
+	}
+}
