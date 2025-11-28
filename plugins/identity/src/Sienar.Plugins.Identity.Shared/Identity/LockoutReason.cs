e242813ed2b8bb78f2b6cc1618908abb8c12f1e3
christianlevesque <christian@levesque.dev>
2025-11-28 15:41:25 -0600

    added missing XMLDoc to LockoutReason

--- plugins/identity/src/Sienar.Plugins.Identity.Shared/Identity/LockoutReason.cs
+++ plugins/identity/src/Sienar.Plugins.Identity.Shared/Identity/LockoutReason.cs
@@ -4,14 +4,26 @@ using Sienar.Data;
 
 namespace Sienar.Identity;
 
+/// <summary>
+/// A reason why a user might be locked out
+/// </summary>
 [EntityName(Singular = "lockout reason", Plural = "lockout reasons")]
 public class LockoutReason : EntityBase
 {
+	/// <summary>
+	/// The reason why a user might be locked out
+	/// </summary>
 	public string Reason { get; set; } = string.Empty;
 
+	/// <summary>
+	/// The normalized reason why a user might be locked out
+	/// </summary>
 	[JsonIgnore]
 	public string NormalizedReason { get; set; } = string.Empty;
 
+	/// <summary>
+	/// The users who are locked out for this reason
+	/// </summary>
 	[JsonIgnore]
 	public List<SienarUser> Users { get; set; } = [];
 
